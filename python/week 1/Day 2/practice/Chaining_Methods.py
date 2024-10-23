c5823c4544935ed85e578454af8bc6e2c3a819d8
YassinBn02 <yassinbn02@gmail.com>
2024-10-23 19:55:32 +0200

    Chaining_Methods update

--- python/week 1/Day 2/practice/Chaining_Methods.py
+++ python/week 1/Day 2/practice/Chaining_Methods.py
@@ -1,4 +1,4 @@
-class users: 
+class user: 
     def __init__(self,first_name,last_name,email,age):
         self.first_name=first_name
         self.last_name=last_name
@@ -7,7 +7,7 @@ class users:
         self.is_rewards_member =False 
         self.gold_card_points = 0 
 
-    def dispaly_info(self):
+    def display_info(self):
         print(f"first name: {self.first_name}")
         print(f"last name: {self.last_name}")
         print(f"email :{self.email}")
@@ -17,16 +17,23 @@ class users:
         return self
 
     def enroll(self):
+        if (self.is_rewards_member==True):
+            print("the user is already a member")
+            return self
+        else:
             self.is_rewards_member=True
             self.gold_card_points=200 
             return self
 
     def spend_points(self, amount):
-        self.gold_card_points=self.gold_card_points - amount
-        return self
+        if (self.gold_card_points < amount):
+            print("Sorry, you don't have enough points")
+        else:
+            self.gold_card_points=self.gold_card_points - amount
+            return self
 
-user1=users("yassin","benabdallah","yasssinbn@gmail.com",22) 
-user2=users("nourhene","amdouni","nourhene803@gamil",21)
+user1=user("yassin","benabdallah","yasssinbn@gmail.com",22) 
+user2=user("nourhene","amdouni","nourhene803@gamil",21)
 
 user1.dispaly_info().enroll().spend_points(50).dispaly_info()
 print("*"*50)
