360caf63c9f6ae31b9e1e64bb9700af2e534e7c9
YassinBn02 <yassinbn02@gmail.com>
2024-10-23 19:43:39 +0200

    for_loop_basic1

--- python/week 1/Day 1/core/for_loop_basic1.py
+++ python/week 1/Day 1/core/for_loop_basic1.py
@@ -4,17 +4,17 @@
 def Basic():
     for i in range (151):
         print(i)
-print(Basic())
+Basic()
 
 print("#"*10)
 
 # Multiples of Five
 
 def Multiples_of_Five():
-    for i in range (5,1001):
-        if i%5 ==0 :
-            print(i)
-print(Multiples_of_Five())
+    for i in range (5,1001,5):
+        print(i)
+
+Multiples_of_Five()
 
 
 #Counting, the Dojo Way
@@ -27,7 +27,7 @@ def Counting_the_Dojo_Way():
             print("Coding")
         else:
             print(i)
-print(Counting_the_Dojo_Way())
+Counting_the_Dojo_Way()
 
 #That_Sucker's_Huge
 print("#"*10)
@@ -35,7 +35,7 @@ print("#"*10)
 def That_Sucker():
     sum=0
     for i in range (500000):
-        if i%2==0:
+        if i%2!=0:
             sum=sum+i
     return sum
 print(That_Sucker())
@@ -45,7 +45,7 @@ print("#"*10)
 def  Countdown_by_Fours():
     for i in range (2018,-1,-4): 
         print(i)
-print(Countdown_by_Fours())
+Countdown_by_Fours()
 
 #Flexible_Counter
 print("#"*10)
@@ -54,4 +54,4 @@ def Flexible_Counter(lowNum,highNum,mult):
     for i in range (lowNum,highNum+1):
         if i%mult==0:
             print(i)
-print(Flexible_Counter(2,9,3))
+Flexible_Counter(2,9,3)
