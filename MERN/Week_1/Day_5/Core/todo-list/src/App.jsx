ee35508151661db9bb041208e7a5cffaf7ac1dd8
YassinBn02 <yassinbn02@gmail.com>
2024-12-01 17:30:56 +0100

    Todo list

---
+++ MERN/Week_1/Day_5/Core/todo-list/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import TodoForm from './components/TodoForm'
+import DisplayTodo from './components/DisplayTodo'
+
+function App() {
+  const [todos,setTodos]=useState([])
+  const add=(obj)=>{
+    setTodos([...todos,obj])
+  }
+  return (
+    <>
+      <div className='container p-5 '>
+        <TodoForm add={add} />
+        <DisplayTodo todos={todos} setTodos={setTodos} />
+      </div>
+    </>
+  )
+}
+
+export default App
