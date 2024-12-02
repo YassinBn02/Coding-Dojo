8fb41922bd708bd5f09b18eec245733f001e20f2
YassinBn02 <yassinbn02@gmail.com>
2024-12-02 13:58:20 +0100

    Axios Pokemon API

---
+++ MERN/Week_2/Day_1/Core/axios-pokemon-api/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  
+  useEffect(()=>{
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+    .then ((response) => setPokemons(response.data.results))
+    .catch(err=>console.log(err))
+  } ,[])
+
+  
+  return (
+    <>
+      <h1>Pokemon API</h1>
+      <ul>
+        {
+          pokemons.map((pokemon,i)=>{
+            return(
+              <li key={i}>{pokemon.name}</li>
+            )
+          })
+        }
+      </ul>
+    </>
+  )
+}
+
+export default App
