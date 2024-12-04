bc005b50614baa57e122ce295d922b5b069ba291
YassinBn02 <yassinbn02@gmail.com>
2024-12-04 18:28:53 +0100

    TV Request

---
+++ MERN/Week_2/Day_3/Practice/TV Request/server/sever.js
@@ -0,0 +1,13 @@
+import express from "express"
+import router from "./routes/tvShows.routes.js"
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+
+
+app.use("/api",router)
+
+
+
+app.listen(PORT, () => console.log(`Server is Running and ready for REQ and RES ${PORT}`))
